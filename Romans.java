3d530afce69d2574562741c8b05ee0d9bb741d9a
ADSivu <aadhav.shiva@gmail.com>
2024-09-23 19:41:12 +0000

    boop

--- Romans.java
+++ Romans.java
@@ -1,23 +1,30 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Aadhav Sivakumar
 
-import java.util.*;
+import java.util.Map;
+import java.util.HashMap;
 
-public class Romans
-{
-	private Map<String, Integer> m;
-	
-	public Romans()
-	{
-		m = new TreeMap<String, Integer>();
-		
-		//complete your map				
-	}
-	
-	public int getNumber( String s )
-	{
-		//complete this method
-	}
+public class Romans {
+    private Map<String, Integer> m;
 
+    // Constructor to initialize the map with Roman numerals and their values
+    public Romans() {
+        m = new HashMap<>();
+        m.put("I", 1);
+        m.put("V", 5);
+        m.put("X", 10);
+        m.put("L", 50);
+        m.put("C", 100);
+        m.put("D", 500);
+        m.put("M", 1000);
+    }
+
+    // Method to return the numeric value of a given Roman numeral
+    public int getValue(String roman) {
+        return m.get(roman);  // Return the value or -1 if the numeral doesn't exist
+    }
 }
+
+
+
---
+++ RomansRunner.java
@@ -0,0 +1,12 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - Aadhav Sivakumar
+
+public class RomansRunner {
+    public static void main(String args[]) {
+        Romans w = new Romans();
+        System.out.println(w.getValue("V"));  
+        System.out.println(w.getValue("D"));  
+        System.out.println(w.getValue("M"));  
+    }
+}
